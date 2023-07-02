40104bbdcd11a5b527a0cc20758ff342e070cc96
atlas <a.sproson@outlook.com>
2023-07-02 08:55:58 +0100

    Builds backend server

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+// Automatically parse any JSON sent to the server
+app.use(express.json());
+
+// Make some animals
+import Chance from "chance";
+
+const chance = new Chance();
+
+const animals = [...Array(250).keys()].map((id) => {
+  return {
+    id,
+    type: chance.animal(),
+    age: chance.age(),
+    name: chance.name(),
+  };
+});
+
+// End point to search for animals
+app.get("", (req, res) => {
+  // Filter results by query
+  const q = req.query.q?.toLowerCase() || "";
+  const results = animals.filter((animal) =>
+    animal.type.toLowerCase().includes(q)
+  );
+
+  res.send(results);
+});
+
+app.listen(8080, () => console.log("Listening on port http://localhost:8080"));
